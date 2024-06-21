3f06c110858860bff6222f9d0aa8d80cbd973326
Tychkcl <tocabloha@gmail.com>
2024-06-21 13:44:04 +0500

    gg

--- tests/pr7.py
+++ tests/pr7.py
@@ -2,6 +2,7 @@ import random
 from pprint import pprint
 from prettytable import PrettyTable
 import matplotlib.pyplot as plt
+import math
 
 #Исходные переменные
 n = 2699
@@ -26,7 +27,7 @@ t.add_column("Pi",list(map(lambda z: str(z)+"/"+str(n), [i for i in x])))
 #Вывод
 print(t)
 print("X - Значение\nNi - Частота\nPi - Частость")
-print(f"Мода: {x.index(max(x))+1}, Медиана: 3, 4")
+print(f"\nМода: {x.index(max(x))+1}, Медиана: 3, 4")
 
 t = list(map(lambda z: str(z+1), range(len(x)))) #массив
 s = x #массив
@@ -38,4 +39,26 @@ for i in x:
 plt.ylim(0,600)
 plt.show()
 
+print("\nСистема F(x):")
+print(f"0, x<0")
+for i in range(0,5):
+    print(f"{sum(x[0:i])}/{n}, {i}<=x<{i+1}")
+print(f"1, 5<=x<6")
+
+gg = 0
+for i in range(5):
+    gg += x[i]*(i+1)
+print(f"\nXв = {round(gg/n,2)}")
+xb = round(gg/n,2)
+
+gg = 0
+for i in range(5):
+    gg += (x[i]**2)*(i+1)
+Db = round((gg/n)-xb**2,2)
+print(f"\nDв = {Db}")
+print(f"\nБв = {math.sqrt(Db)}")
+print(f"\nS2 = {n/(n-1)*Db}")
+print(f"\nS = {math.sqrt(n/(n-1)*Db)}")
+print(f"\nV = {(math.sqrt(Db))/xb}")
+print(f"\nd = {(math.sqrt(Db))/xb}")
 #https://matplotlib.online/project?id=0
